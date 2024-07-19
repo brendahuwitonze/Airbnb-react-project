de26a733ef10a5021d2feb42c40870e7d2ef5316
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-07-19 17:14:10 +0200

    used props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Hero from './Hero'
 import Navbar from './navBar'
 import Card from './card'
-
 
 function App() {
  
@@ -14,9 +11,35 @@ function App() {
      <Navbar/>
     <div className='container'>
     <Hero/>
-    <Card/>
+    <div className='mainCard'>
+    <Card
+    status="SOLD OUT"
+    img="src/assets/image.png"
+    imges="src/assets/Star 1.png"
+    views='5.0(6).USA'
+    name='life lesson with katie zaferes'
+    price ="$136/person"
+    />
+ 
+<Card
+    status="ONLINE"
+    img="src/assets/wedding-photography 1.png"
+    imges="src/assets/Star 1.png"
+    views='5.0(6).USA'
+    name='life wedding photography'
+    price ="$125/person"
+    />
+
+<Card
+    status="ONLINE"
+    img="src/assets/mountain-bike 1.png"
+    imges="src/assets/Star 1.png"
+    views='5.0(6).USA'
+    name="Group Mountain Biking"
+    price ="$50/person"
+    /> 
     </div>
-   
+    </div>
     </>
   )
 }
--- src/card.jsx
+++ src/card.jsx
@@ -1,63 +1,26 @@
 import React from "react";
 
-export default function Card(){
-return(
+export default function Card(props) {
+  return (
     <>
-    <div className="cards">
-    <div className="cards1">
-     <div className="img1">
-     <p>SOLD OUT</p>
-     <img className="cardImages"  src="src/assets/image.png" alt="" />
-     </div>
-     <div className="words">
-        <div className="subwords">
-        <img src="src/assets/Star 1.png" alt="" />
-        <p>5.0(6).USA</p>
+      <div className="cards">
+        <div className="cards1">
+          <div className="img1">
+            <p>{props.status}</p>
+            <img className="cardImages" src={props.img} alt="" />
+          </div>
+          <div className="words">
+            <div className="subwords">
+              <img src={props.imges} alt="" />
+              <p>{props.views}</p>
+            </div>
+            <div className="wordsAfter">
+              <h2>{props.name}</h2>
+              <p>{props.price}</p>
+            </div>
+          </div>
         </div>
-        <div className="wordsAfter">
-        <p>Life lessons with Katie Zaferes</p>
-        <p><span>From $136</span>/ person</p>
-    </div>
-     </div>
-    </div>
-
-    <div className="cards2">
-     <div className="card2--img1">
-     <p>SOLD OUT</p>
-     <img className="cardImages"  src="src/assets/wedding-photography 1.png" alt="" />
-     </div>
-     <div className="card2--words">
-        <div className="card2--subwords">
-        <img src="src/assets/Star 1.png" alt="" />
-        <p>5.0(6).USA</p>
-        </div>
-        <div className="card2--wordsAfter">
-        <p>Learn wedding photography</p>
-        <p><span>From $136</span>/ person</p>
-    </div>
-     </div>
-    </div>
-
-
-    <div className="cards3">
-     <div className="card3--img1">
-     <p>SOLD OUT</p>
-     <img className="cardImages" src="src/assets/mountain-bike 1.png" alt="" />
-     </div>
-     <div className="card3--words">
-        <div className="card3--subwords">
-        <img src="src/assets/Star 1.png" alt="" />
-        <p>5.0(6).USA</p>
-        </div>
-        <div className="card3--wordsAfter">
-        <p>Group Mountain Biking</p>
-        <p><span>From $136</span>/ person</p>
-    </div>
-     </div>
-    </div>
-
-
-    </div>
+      </div>
     </>
-)
+  );
 }
